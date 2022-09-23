b344f50cf8f8bc76d04b85c5535320f543e6aa75
Javier Tortosa <americandepress6@gmail.com>
2022-09-23 20:22:01 +0200

    Actualizacion 2022-09-23

---
+++ ejercicio2.openbootcamp/src/com/ejercicio2/openbootcamp/FuncionIva.java
@@ -0,0 +1,19 @@
+package com.ejercicio2.openbootcamp;
+
+public class FuncionIva {
+
+	public static void main(String[] args) {
+
+		double base = 100;
+		double total=0;
+		total = calculaIva(base);
+		
+		System.out.println("El total para la base " + base + " es: " + total);
+		
+	}
+
+	public static double calculaIva(double base) {
+		double total = base * 1.21;
+		return total;
+	}
+}
